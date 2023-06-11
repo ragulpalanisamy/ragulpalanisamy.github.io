ab1243dbca24558f646d5e4753094020eb9854ee
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-06-11 10:18:04 +0530

    feat: :sparkles: created a component for guna cafe

---
+++ client/General/src/GunaCafe/Desserts.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Desserts() {
+  return (
+    <section>
+      <h2>Desserts</h2>
+      <img
+        src="https://cdn.freecodecamp.org/curriculum/css-cafe/pie.jpg"
+        alt="pie icon"
+      />
+      <article className="item">
+        <p className="dessert">Donut</p>
+        <p className="price">1.50</p>
+      </article>
+      <article className="item">
+        <p className="dessert">Cherry Pie</p>
+        <p className="price">2.75</p>
+      </article>
+      <article className="item">
+        <p className="dessert">Cheesecake</p>
+        <p className="price">3.00</p>
+      </article>
+      <article className="item">
+        <p className="dessert">Cinnamon Roll</p>
+        <p className="price">2.50</p>
+      </article>
+    </section>
+  );
+}
--- client/General/src/GunaCafe/guna.tsx
+++ client/General/src/GunaCafe/guna.tsx
@@ -1,5 +1,23 @@
 import React from "react";
 
+import Coffee from "./coffee";
+import Dessert from "./Desserts";
+import Footer from "./Footer";
+
 export default function guna() {
-  return <div>guna</div>;
+  return (
+    <>
+      <div className="menu">
+        <main>
+          <h1>GUNA CAFE</h1>
+          <p className="established">Est. 2020</p>
+          <hr />
+          <Coffee />
+          <Dessert />
+        </main>
+        <hr className="bottom-line" />
+        <Footer />
+      </div>
+    </>
+  );
 }
