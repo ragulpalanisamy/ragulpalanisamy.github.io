e2fe3f0127c4547dceed83d91df81c51c3eaec40
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-10-22 14:14:30 +0530

    feat: :sparkles: Added the structure for the penguin

--- client/General/src/ResponsiveDesigns/buildingPenguin/buildingPenguin.tsx
+++ client/General/src/ResponsiveDesigns/buildingPenguin/buildingPenguin.tsx
@@ -2,6 +2,32 @@ import React from 'react'
 
 export default function buildingPenguin() {
   return (
-    <div className="ground"></div>
+    <><div className="left-mountain"></div><div className="back-mountain"></div><div className="sun"></div><div className="penguin">
+      <div className="penguin-head">
+        <div className="face left"></div>
+        <div className="face right"></div>
+        <div className="chin"></div>
+        <div className="eye left">
+          <div className="eye-lid"></div>
+        </div>
+        <div className="eye right">
+          <div className="eye-lid"></div>
+        </div>
+        <div className="blush left"></div>
+        <div className="blush right"></div>
+        <div className="beak top"></div>
+        <div className="beak bottom"></div>
+      </div>
+      <div className="shirt">
+        <div>💜</div>
+        <p>I CSS</p>
+      </div>
+      <div className="penguin-body">
+        <div className="arm left"></div>
+        <div className="arm right"></div>
+        <div className="foot left"></div>
+        <div className="foot right"></div>
+      </div>
+    </div><div className="ground"></div></>
   )
 }
